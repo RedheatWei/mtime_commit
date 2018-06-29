dfda2fde9c38171bd29c62ef20e0830c043cad8f
RedheatWei <qjyyn@qq.com>
2018-06-29 19:40:30 +0800

    change some

--- mtime_get.py
+++ mtime_get.py
@@ -5,23 +5,64 @@
 @create = '2018/6/28 18:15'
 @email = 'qjyyn@qq.com'
 '''
-import requests
-from bs4 import BeautifulSoup
-import re
+from selenium import webdriver
+import selenium.webdriver.chrome.service as chrome_service
 import sqlite3
+import time
 
-def get_url_list():
-    a_list = []
-    for i in range(1,166):
-        url = 'http://movie.mtime.com/movie/search/section/?year=2017&pageIndex=%s' % i
-        print(url)
-        r = requests.get(url)
-        soup = BeautifulSoup(r.content,'lxml')
-        all_a = soup.find_all(href=re.compile("\d{5,}"))
-        for a in all_a:
-            a_list.append(a.get('href'))
-        return a_list
-a_list = get_url_list()
 
-def insertIntoDb():
-    conn = sqlite3.connect('test.db')
+class OpenUrl(object):
+    def __init__(self):
+        service = chrome_service.Service('/Users/Redheat/Library/chromedriver/chromedriver')
+        service.start()
+        capabilities = {'chrome.binary': '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'}
+        self.driver = webdriver.Remote(service.service_url, capabilities)
+
+    def open_url(self, page):
+        self.driver.get('http://movie.mtime.com/movie/search/section/?year=2017&pageIndex=%s' % page)
+        urls = self.driver.find_elements_by_xpath("//h3[@class='normal mt6']/a")
+        return [url.get_attribute("href") for url in urls]
+    def __del__(self):
+        self.driver.quit()
+
+class ProcessDb(object):
+    def __init__(self,dbname):
+        self.conn = sqlite3.connect(dbname)
+        self.cursor = self.conn.cursor()
+    def insert(self,url_list):
+        for url in url_list:
+            self.cursor.execute('insert or ignore  into urls (url, time_year) values ("%s","%s")' % (url, 2017))
+    def __del__(self):
+        self.cursor.close()
+        self.conn.commit()
+        self.conn.close()
+# def open_url(driver, page):
+#     driver.get('http://movie.mtime.com/movie/search/section/?year=2017&pageIndex=%s' % 1)
+#     urls = driver.find_elements_by_xpath("//h3[@class='normal mt6']/a")
+#     return [url.get_attribute("href") for url in urls]
+
+
+# def insertIntoDb(url_list):
+#     conn = sqlite3.connect('mtime.db')
+#     cursor = conn.cursor()
+#     for url in url_list:
+#         cursor.execute('insert or ignore  into urls (url, time_year) values (%s,%s)' % (url, 2017))
+#     cursor.close()
+#     conn.commit()
+#     conn.close()
+
+
+# def create_dirver():
+#     service = chrome_service.Service('/Users/Redheat/Library/chromedriver/chromedriver')
+#     service.start()
+#     capabilities = {'chrome.binary': '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'}
+#     dirver = webdriver.Remote(service.service_url, capabilities)
+#     return dirver
+
+driver = OpenUrl()
+db = ProcessDb("mtime.db3")
+for page in range(8, 166):
+    print(page)
+    time.sleep(3)
+    url_list = driver.open_url(page)
+    db.insert(url_list)
